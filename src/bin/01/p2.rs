c42e61749751638bbf2719b45ae1040025b43b3c
Chris Holder <chris.holder@ramseysolutions.com>
2022-12-01 09:15:54 -0600

    Complete day 1, part 2

--- src/bin/01/p2.rs
+++ src/bin/01/p2.rs
@@ -1,1 +1,20 @@
-pub fn solve() {}
+pub fn solve() -> u32 {
+    let cals = super::parse_input();
+    let mut sums: Vec<u32> = Vec::new();
+    let mut sum = 0;
+
+    for c in cals.lines() {
+        if c.is_empty() {
+            sums.push(sum);
+            sum = 0;
+        } else {
+            let c_num: u32 = c.parse().expect("Failed to parse");
+            sum += c_num;
+        }
+    }
+
+    sums.sort();
+
+    let top = &sums[sums.len() - 3..];
+    top.iter().sum()
+}
