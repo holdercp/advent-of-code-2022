f69a77e5135ed5d89e7bf9a8c84f6def6e7bdd8c
Chris Holder <chris.holder@ramseysolutions.com>
2022-12-01 09:15:44 -0600

    Complete day 1, part 1

--- src/bin/01/main.rs
+++ src/bin/01/main.rs
@@ -1,7 +1,16 @@
+use std::fs;
+
 pub mod p1;
 pub mod p2;
 
+fn parse_input() -> String {
+    let cals =
+        fs::read_to_string("src/bin/01/input.txt").expect("Should have been able to read the file");
+
+    cals
+}
+
 fn main() {
-    // println!("Part 1: {}", p1::solve());
-    // println!("Part 2: {}", p2::solve());
+    println!("Part 1: {}", p1::solve());
+    println!("Part 2: {}", p2::solve());
 }
--- src/bin/01/p1.rs
+++ src/bin/01/p1.rs
@@ -1,1 +1,20 @@
-pub fn solve() {}
+pub fn solve() -> u32 {
+    let cals = super::parse_input();
+    let mut greatest = 0;
+    let mut sum = 0;
+
+    for c in cals.lines() {
+        if c.is_empty() {
+            if sum > greatest {
+                greatest = sum;
+            }
+
+            sum = 0;
+        } else {
+            let c_num: u32 = c.parse().expect("Failed to parse");
+            sum += c_num;
+        }
+    }
+
+    greatest
+}
