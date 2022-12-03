51b990cd0ff033511dd82a550d323c6a08f6394b
holdercp <25958393+holdercp@users.noreply.github.com>
2022-12-02 19:10:57 -0600

    Refactor day 2,  part 1

--- src/bin/02/p1.rs
+++ src/bin/02/p1.rs
@@ -1,48 +1,73 @@
-fn get_shape_score(shape: &str) -> u32 {
+enum Shape {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+struct Round {
+    opponent_shape: Shape,
+    my_shape: Shape,
+}
+
+fn calc_shape_score(shape: &Shape) -> i32 {
     match shape {
-        "X" => 1,
-        "Y" => 2,
-        "Z" => 3,
+        Shape::Rock => 1,
+        Shape::Paper => 2,
+        Shape::Scissors => 3,
+    }
+}
+
+fn calc_outcome_score(round: &Round) -> i32 {
+    match round.opponent_shape {
+        Shape::Rock => match round.my_shape {
+            Shape::Rock => 3,
+            Shape::Paper => 6,
+            Shape::Scissors => 0,
+        },
+        Shape::Paper => match round.my_shape {
+            Shape::Rock => 0,
+            Shape::Paper => 3,
+            Shape::Scissors => 6,
+        },
+        Shape::Scissors => match round.my_shape {
+            Shape::Rock => 6,
+            Shape::Paper => 0,
+            Shape::Scissors => 3,
+        },
+    }
+}
+
+fn create_shape(shape_raw: &str) -> Shape {
+    match shape_raw {
+        "A" => Shape::Rock,
+        "B" => Shape::Paper,
+        "C" => Shape::Scissors,
+        "X" => Shape::Rock,
+        "Y" => Shape::Paper,
+        "Z" => Shape::Scissors,
         _ => panic!("bad shape"),
     }
 }
 
-fn get_outcome_score(p1: &str, p2: &str) -> u32 {
-    match p1 {
-        "A" => match p2 {
-            "X" => 3,
-            "Y" => 6,
-            "Z" => 0,
-            _ => panic!("bad shape"),
-        },
-        "B" => match p2 {
-            "X" => 0,
-            "Y" => 3,
-            "Z" => 6,
-            _ => panic!("bad shape"),
-        },
-        "C" => match p2 {
-            "X" => 6,
-            "Y" => 0,
-            "Z" => 3,
-            _ => panic!("bad shape"),
-        },
-        _ => panic!("bad shape"),
-    }
-}
-
-pub fn solve() -> u32 {
+pub fn solve() -> i32 {
     let guide = super::read_input();
 
-    let mut score: u32 = 0;
+    let create_round = |line: &str| {
+        let cols: Vec<&str> = line.split_whitespace().collect();
+        Round {
+            opponent_shape: create_shape(cols[0]),
+            my_shape: create_shape(cols[1]),
+        }
+    };
 
-    for l in guide.lines() {
-        let shapes: Vec<&str> = l.split_whitespace().collect();
-        let outcome_score = get_outcome_score(shapes[0], shapes[1]);
-        let shape_score = get_shape_score(shapes[1]);
+    let calc_score = |total, round: Round| {
+        let outcome_score = calc_outcome_score(&round);
+        let shape_score = calc_shape_score(&round.my_shape);
 
-        score += outcome_score + shape_score;
-    }
+        total + outcome_score + shape_score
+    };
 
-    score
+    let total = guide.lines().map(create_round).fold(0, calc_score);
+
+    total
 }
