09a90b3e2d7b972a1215f38fb7426c3dcff4ff9e
holdercp <25958393+holdercp@users.noreply.github.com>
2022-12-04 01:16:19 -0600

    Complete day 4, part 1

--- src/bin/04/main.rs
+++ src/bin/04/main.rs
@@ -11,3 +11,39 @@ fn main() {
     println!("Part 1: {}", p1::solve());
     println!("Part 2: {}", p2::solve());
 }
+
+struct Pair {
+    first: Assignment,
+    second: Assignment,
+}
+
+impl Pair {
+    fn build(p: &str) -> Pair {
+        let assignments: Vec<&str> = p.split(",").collect();
+
+        Pair {
+            first: Assignment::build(assignments[0]),
+            second: Assignment::build(assignments[1]),
+        }
+    }
+}
+
+struct Assignment {
+    start: u32,
+    end: u32,
+}
+
+impl Assignment {
+    fn build(a: &str) -> Assignment {
+        let sections: Vec<u32> = a.split("-").flat_map(|s| s.parse()).collect();
+
+        Assignment {
+            start: sections[0],
+            end: sections[1],
+        }
+    }
+
+    fn contains(&self, other: &Assignment) -> bool {
+        self.start <= other.start && self.end >= other.end
+    }
+}
--- src/bin/04/p1.rs
+++ src/bin/04/p1.rs
@@ -1,3 +1,13 @@
-pub fn solve() -> String {
-    String::from("Not implemented")
+pub fn solve() -> u32 {
+    let input = super::read_input();
+
+    let contain_pairs: u32 = input.lines().map(super::Pair::build).fold(0, |count, p| {
+        if p.first.contains(&p.second) || p.second.contains(&p.first) {
+            return count + 1;
+        }
+
+        count
+    });
+
+    contain_pairs
 }
