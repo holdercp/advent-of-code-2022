e6d24cbfae55dc756e4b93ec78032b0bab4eac34
Chris Holder <chris.holder@ramseysolutions.com>
2022-12-05 12:51:18 -0600

    Complete day 5, part 1

--- src/bin/05/main.rs
+++ src/bin/05/main.rs
@@ -3,11 +3,56 @@ use std::fs;
 pub mod p1;
 pub mod p2;
 
-fn read_input() -> String {
-    fs::read_to_string("src/bin/05/input.txt").expect("Should have been able to read the file")
+fn build_supply(raw: &str) -> Vec<Vec<char>> {
+    let mut lines: Vec<&str> = raw.lines().collect();
+
+    // We don't care about the stack num from the input
+    lines.pop();
+
+    lines.reverse();
+
+    let mut supply: Vec<Vec<char>> = vec![Vec::new(); 9];
+
+    for line in lines {
+        let cols: Vec<char> = line.chars().collect();
+        let chunks = cols.chunks(4);
+
+        for (i, chunk) in chunks.enumerate() {
+            if chunk[0] == '[' {
+                supply[i].push(chunk[1]);
+            }
+        }
+    }
+
+    supply
+}
+
+fn build_instructions(raw: &str) -> Vec<(u32, usize, usize)> {
+    let mut instructions: Vec<(u32, usize, usize)> = Vec::new();
+
+    for line in raw.lines() {
+        let nums: Vec<u32> = line.split_whitespace().flat_map(|w| w.parse()).collect();
+
+        instructions.push((nums[0], nums[1] as usize, nums[2] as usize));
+    }
+
+    instructions
+}
+
+fn parse_input() -> (Vec<Vec<char>>, Vec<(u32, usize, usize)>) {
+    let input =
+        fs::read_to_string("src/bin/05/input.txt").expect("Should have been able to read the file");
+
+    let input: Vec<&str> = input.split("\n\n").collect();
+
+    let supply = build_supply(input[0]);
+    let instructions = build_instructions(input[1]);
+
+    (supply, instructions)
 }
 
 fn main() {
+    parse_input();
     println!("Part 1: {}", p1::solve());
     println!("Part 2: {}", p2::solve());
 }
--- src/bin/05/p1.rs
+++ src/bin/05/p1.rs
@@ -1,3 +1,19 @@
 pub fn solve() -> String {
-    String::from("Not implemented")
+    let (mut supply, procedure) = super::parse_input();
+
+    for instruction in procedure {
+        for _time in 0..instruction.0 {
+            println!("{:?}", instruction);
+            let c = supply[instruction.1 - 1].pop().unwrap();
+            supply[instruction.2 - 1].push(c);
+        }
+    }
+
+    let mut top = String::new();
+    for mut krates in supply {
+        let c = krates.pop().unwrap();
+        top.push(c);
+    }
+
+    top
 }
