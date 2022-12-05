4b2127f8fa7c3999f9590c6728de1591a84cd49d
Chris Holder <chris.holder@ramseysolutions.com>
2022-12-05 12:51:32 -0600

    Complete day 5, part 2

--- src/bin/05/p2.rs
+++ src/bin/05/p2.rs
@@ -1,3 +1,20 @@
 pub fn solve() -> String {
-    String::from("Not implemented")
+    let (mut supply, procedure) = super::parse_input();
+
+    for instruction in procedure {
+        let mut temp: Vec<char> = Vec::new();
+        for _time in 0..instruction.0 {
+            let c = supply[instruction.1 - 1].pop().unwrap();
+            temp.push(c);
+        }
+        supply[instruction.2 - 1].extend(temp.iter().rev());
+    }
+
+    let mut top = String::new();
+    for mut krates in supply {
+        let c = krates.pop().unwrap();
+        top.push(c);
+    }
+
+    top
 }
