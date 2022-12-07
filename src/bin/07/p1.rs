9cfd31930e876b49e1e4d43e1b3c3309bbf30313
Chris Holder <chris.holder@ramseysolutions.com>
2022-12-07 16:43:38 -0600

    Complete day 7, part 1

--- src/bin/07/p1.rs
+++ src/bin/07/p1.rs
@@ -1,3 +1,37 @@
-pub fn solve() -> String {
-    String::from("Not implemented")
+use std::collections::HashMap;
+
+pub fn solve() -> u32 {
+    let input = super::read_input();
+
+    let mut history: Vec<String> = Vec::new();
+    let mut sums: HashMap<String, u32> = HashMap::new();
+
+    for line in input.lines() {
+        if line.starts_with("$ cd ") {
+            let arg = &line[5..];
+
+            if arg == ".." {
+                history.pop();
+            } else {
+                let mut dir = String::new();
+                for d in history.clone() {
+                    dir.push_str(&d);
+                }
+                dir.push_str(arg);
+                history.push(dir);
+            }
+        }
+
+        if line.starts_with(|c: char| c.is_numeric()) {
+            let split: Vec<&str> = line.split_whitespace().collect();
+            let size: u32 = split[0].parse().unwrap();
+
+            for dir in history.clone() {
+                let sum = sums.entry(dir).or_insert(0);
+                *sum += size;
+            }
+        }
+    }
+
+    sums.into_values().filter(|v| v <= &100000).sum()
 }
