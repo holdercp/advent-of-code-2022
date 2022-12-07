6bb5e17bdc5718ec390baff803437a8f74148d29
Chris Holder <chris.holder@ramseysolutions.com>
2022-12-07 16:43:49 -0600

    Complete day 7, part 2

--- src/bin/07/p2.rs
+++ src/bin/07/p2.rs
@@ -1,3 +1,44 @@
-pub fn solve() -> String {
-    String::from("Not implemented")
+use std::collections::HashMap;
+
+const TOTAL_SIZE: u32 = 70_000_000;
+const REQUIRED_SIZE: u32 = 30_000_000;
+
+pub fn solve() -> u32 {
+    let input = super::read_input();
+
+    let mut history: Vec<String> = Vec::new();
+    let mut sums: HashMap<String, u32> = HashMap::new();
+
+    for line in input.lines() {
+        if line.starts_with("$ cd ") {
+            let arg = &line[5..];
+
+            if arg == ".." {
+                history.pop();
+            } else {
+                let mut dir = String::new();
+                for d in history.clone() {
+                    dir.push_str(&d);
+                }
+                dir.push_str(arg);
+                history.push(dir);
+            }
+        }
+
+        if line.starts_with(|c: char| c.is_numeric()) {
+            let split: Vec<&str> = line.split_whitespace().collect();
+            let size: u32 = split[0].parse().unwrap();
+
+            for dir in history.clone() {
+                let sum = sums.entry(dir).or_insert(0);
+                *sum += size;
+            }
+        }
+    }
+
+    let used_space = sums.get("/").unwrap();
+    let free_space = TOTAL_SIZE - used_space;
+    let needed_space = REQUIRED_SIZE - free_space;
+
+    *sums.values().filter(|v| v >= &&needed_space).min().unwrap()
 }
