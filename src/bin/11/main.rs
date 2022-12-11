2cb18b212e31e5422e545b133939b4173814245b
holdercp <25958393+holdercp@users.noreply.github.com>
2022-12-11 16:23:51 -0600

    Complete day 11, part 1

--- src/bin/11/main.rs
+++ src/bin/11/main.rs
@@ -1,13 +1,173 @@
-use std::fs;
+use std::{
+    collections::{HashMap, VecDeque},
+    fs,
+};
 
 pub mod p1;
 pub mod p2;
 
-fn read_input() -> String {
-    fs::read_to_string("src/bin/11/input.txt").expect("Should have been able to read the file")
+fn parse_input() -> HashMap<usize, Monkey> {
+    let input =
+        fs::read_to_string("src/bin/11/input.txt").expect("Should have been able to read the file");
+
+    let mut monkeys = HashMap::new();
+
+    input.split("\n\n").enumerate().for_each(|(i, m)| {
+        monkeys.insert(i, Monkey::build(m));
+    });
+
+    monkeys
 }
 
 fn main() {
+    parse_input();
     println!("Part 1: {}", p1::solve());
     println!("Part 2: {}", p2::solve());
+}
+
+#[derive(Debug)]
+enum OperationType {
+    Multiply,
+    Add,
+}
+
+#[derive(Debug)]
+enum Operand {
+    Integer(u32),
+    Item,
+}
+
+#[derive(Debug)]
+struct Operation(OperationType, Operand, Operand);
+
+impl Operation {
+    fn build(raw: &str) -> Operation {
+        let op_raw = &raw[17..];
+
+        let op: Vec<&str> = op_raw.split_whitespace().collect();
+
+        let opd1 = match op[0] {
+            "old" => Operand::Item,
+            other => Operand::Integer(other.parse().unwrap()),
+        };
+
+        let opd2 = match op[2] {
+            "old" => Operand::Item,
+            other => Operand::Integer(other.parse().unwrap()),
+        };
+
+        let opt = match op[1] {
+            "*" => OperationType::Multiply,
+            "+" => OperationType::Add,
+            _ => panic!("bad operation type"),
+        };
+
+        Operation(opt, opd1, opd2)
+    }
+
+    fn execute(&self, item: &Item) -> u32 {
+        let Operation(opt, opd1, opd2) = self;
+
+        match opt {
+            OperationType::Multiply => {
+                let a = match opd1 {
+                    Operand::Integer(int) => int,
+                    Operand::Item => &item.worry,
+                };
+                let b = match opd2 {
+                    Operand::Integer(int) => int,
+                    Operand::Item => &item.worry,
+                };
+
+                a * b
+            }
+            OperationType::Add => {
+                let a = match opd1 {
+                    Operand::Integer(int) => int,
+                    Operand::Item => &item.worry,
+                };
+                let b = match opd2 {
+                    Operand::Integer(int) => int,
+                    Operand::Item => &item.worry,
+                };
+
+                a + b
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Test {
+    divisor: u32,
+    t: usize,
+    f: usize,
+}
+
+impl Test {
+    fn build(raw: Vec<&str>) -> Test {
+        let divisor: u32 = raw[0][19..].parse().unwrap();
+        let t: usize = raw[1][25..].parse().unwrap();
+        let f: usize = raw[2][26..].parse().unwrap();
+
+        Test { divisor, t, f }
+    }
+}
+
+#[derive(Debug)]
+struct Monkey {
+    items: VecDeque<Item>,
+    operation: Operation,
+    test: Test,
+    inspected: u32,
+}
+
+impl Monkey {
+    fn build(raw: &str) -> Monkey {
+        let lines: Vec<&str> = raw.lines().map(|l| l.trim()).collect();
+
+        let items = Item::build(lines[1]);
+        let operation = Operation::build(lines[2]);
+        let test = Test::build(lines[3..].to_vec());
+
+        Monkey {
+            items,
+            operation,
+            test,
+            inspected: 0,
+        }
+    }
+
+    fn inspect_item(&mut self) -> Item {
+        self.inspected += 1;
+        self.items.pop_front().unwrap()
+    }
+
+    fn test(&self, item: &Item) -> usize {
+        if item.worry % self.test.divisor == 0 {
+            self.test.t
+        } else {
+            self.test.f
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Item {
+    worry: u32,
+}
+
+impl Item {
+    fn new(worry: u32) -> Self {
+        Self { worry }
+    }
+
+    fn build(raw: &str) -> VecDeque<Item> {
+        let items_raw = &raw[16..];
+        items_raw
+            .split(", ")
+            .flat_map(|n| n.parse::<u32>())
+            .map(Item::new)
+            .collect()
+    }
 }
--- src/bin/11/p1.rs
+++ src/bin/11/p1.rs
@@ -1,3 +1,36 @@
-pub fn solve() -> String {
-    String::from("Not implemented")
+const ROUNDS: usize = 20;
+
+pub fn solve() -> u32 {
+    let mut monkeys = super::parse_input();
+
+    let lcm: u64 = 9_699_690;
+
+    for _ in 0..ROUNDS {
+        for id in 0..monkeys.len() {
+            let mut monkey = monkeys.remove(&id).unwrap();
+
+            while !monkey.items.is_empty() {
+                let mut item = monkey.inspect_item();
+
+                let new_worry = monkey.operation.execute(&item);
+
+                let new_worry = new_worry / 3;
+
+                item.worry = new_worry;
+
+                let other_id = monkey.test(&item);
+
+                monkeys.get_mut(&other_id).unwrap().items.push_back(item);
+            }
+
+            monkeys.insert(id, monkey);
+        }
+    }
+
+    let mut inspected = monkeys.values().map(|m| m.inspected).collect::<Vec<u32>>();
+    inspected.sort();
+
+    let largest = &inspected[inspected.len() - 2..];
+
+    largest.iter().product()
 }
