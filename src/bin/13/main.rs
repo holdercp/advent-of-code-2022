91dc9e52fb030bf9a883dd8379e60771ccbbf4e0
Chris Holder <chris.holder@ramseysolutions.com>
2022-12-13 17:03:31 -0600

    Solve day 13, part 1

--- src/bin/13/main.rs
+++ src/bin/13/main.rs
@@ -1,13 +1,34 @@
 use std::fs;
 
+use serde_json::Value;
+
 pub mod p1;
 pub mod p2;
 
-fn read_input() -> String {
-    fs::read_to_string("src/bin/13/input.txt").expect("Should have been able to read the file")
+fn parse_input() -> Vec<Pair> {
+    let input =
+        fs::read_to_string("src/bin/13/input.txt").expect("Should have been able to read the file");
+
+    input
+        .split("\n\n")
+        .map(|pair| {
+            let packets: Vec<&str> = pair.split_whitespace().collect();
+
+            Pair {
+                left: serde_json::from_str(packets[0]).unwrap(),
+                right: serde_json::from_str(packets[1]).unwrap(),
+            }
+        })
+        .collect()
 }
 
 fn main() {
     println!("Part 1: {}", p1::solve());
     println!("Part 2: {}", p2::solve());
+}
+
+#[derive(Debug)]
+struct Pair {
+    left: Value,
+    right: Value,
 }
--- src/bin/13/p1.rs
+++ src/bin/13/p1.rs
@@ -1,3 +1,115 @@
-pub fn solve() -> String {
-    String::from("Not implemented")
+use crate::{parse_input, Pair};
+
+pub fn solve() -> usize {
+    let pairs = parse_input();
+    let mut sum = 0;
+
+    for (i, p) in pairs.iter().enumerate() {
+        let res = is_sorted_pair(p);
+
+        match res {
+            State::Sorted => {
+                sum += i + 1;
+            }
+            _ => (),
+        };
+    }
+
+    sum
+}
+
+fn is_sorted_pair(p: &Pair) -> State {
+    let mut index = 0;
+
+    let left_packet = p.left.as_array().unwrap();
+    let right_packet = p.right.as_array().unwrap();
+
+    loop {
+        // We've reached the end of both packets
+        if index >= left_packet.len() && index >= right_packet.len() {
+            return State::Indeterminate;
+        }
+
+        if index >= left_packet.len() {
+            return State::Sorted;
+        }
+
+        if index >= right_packet.len() {
+            return State::Unsorted;
+        }
+
+        let left = &p.left[index];
+        let right = &p.right[index];
+
+        if left.is_number() && right.is_number() {
+            let left = left.as_u64().unwrap();
+            let right = right.as_u64().unwrap();
+
+            if left < right {
+                return State::Sorted;
+            } else if left > right {
+                return State::Unsorted;
+            }
+
+            index += 1;
+            continue;
+        }
+
+        if left.is_number() {
+            let left = serde_json::to_value(vec![left]).unwrap();
+            let res = is_sorted_pair(&Pair {
+                left,
+                right: right.to_owned(),
+            });
+
+            match res {
+                State::Indeterminate => {
+                    index += 1;
+                    continue;
+                }
+                _ => {
+                    return res;
+                }
+            };
+        }
+
+        if right.is_number() {
+            let right = serde_json::to_value(vec![right]).unwrap();
+            let res = is_sorted_pair(&Pair {
+                left: left.to_owned(),
+                right,
+            });
+
+            match res {
+                State::Indeterminate => {
+                    index += 1;
+                    continue;
+                }
+                _ => {
+                    return res;
+                }
+            };
+        }
+
+        let res = is_sorted_pair(&Pair {
+            left: left.to_owned(),
+            right: right.to_owned(),
+        });
+
+        match res {
+            State::Indeterminate => {
+                index += 1;
+                continue;
+            }
+            _ => {
+                return res;
+            }
+        };
+    }
+}
+
+enum State {
+    Sorted,
+    Unsorted,
+    Indeterminate,
 }
