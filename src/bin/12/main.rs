bef8e3cdca5abc31dba3a7e0e867d84d47dc97f9
Chris Holder <chris.holder@ramseysolutions.com>
2022-12-13 07:48:04 -0600

    Solve day 12, part one... poorly

--- src/bin/12/main.rs
+++ src/bin/12/main.rs
@@ -3,34 +3,107 @@ use std::{collections::HashMap, fs};
 pub mod p1;
 pub mod p2;
 
-fn parse_input() -> HashMap<char, HashMap<char, u32>> {
+fn parse_input() -> (
+    (usize, usize),
+    (usize, usize),
+    HashMap<(usize, usize), Vec<(usize, usize)>>,
+) {
     let input =
         fs::read_to_string("src/bin/12/input.txt").expect("Should have been able to read the file");
 
-    let row_size = input.find("\n").unwrap();
+    let grid: Vec<Vec<char>> = input.lines().map(|l| l.trim().chars().collect()).collect();
 
-    let line: Vec<char> = input.lines().flat_map(|l| l.trim().chars()).collect();
+    build_graph(&grid)
+}
 
-    let graph: HashMap<char, HashMap<char, u32>> = HashMap::new();
+fn build_graph(
+    grid: &Vec<Vec<char>>,
+) -> (
+    (usize, usize),
+    (usize, usize),
+    HashMap<(usize, usize), Vec<(usize, usize)>>,
+) {
+    let h_bound = grid[0].len() - 1;
+    let v_bound = grid.len() - 1;
 
-    for (i, c) in line.iter().enumerate() {
-        let up = i - row_size;
-        let down = i + row_size;
-        let left = i - 1;
-        let right = i + 1;
+    let mut highest: (usize, usize) = (0, 0);
+    let mut start: (usize, usize) = (0, 0);
 
-        let neighbors: HashMap<char, u32> = HashMap::new();
+    let mut graph: HashMap<(usize, usize), Vec<(usize, usize)>> = HashMap::new();
 
-        if up >= 0 {}
+    for (y, row) in grid.iter().enumerate() {
+        for (x, e) in row.iter().enumerate() {
+            let elevation = &grid[y][x];
 
-        if down >= 0 {}
+            let mut neighbors: Vec<(usize, usize)> = Vec::new();
 
-        if left >= 0 {}
+            if y > 0 && can_traverse(elevation, &grid[y - 1][x]) {
+                neighbors.push((x, y - 1));
+            }
 
-        if right >= 0 {}
+            if y < v_bound && can_traverse(elevation, &grid[y + 1][x]) {
+                neighbors.push((x, y + 1));
+            }
+
+            if x > 0 && can_traverse(elevation, &grid[y][x - 1]) {
+                neighbors.push((x - 1, y));
+            }
+
+            if x < h_bound && can_traverse(elevation, &grid[y][x + 1]) {
+                neighbors.push((x + 1, y));
+            }
+
+            graph.insert((x, y), neighbors);
+
+            if *elevation == 'E' {
+                highest = (x, y)
+            }
+
+            if *elevation == 'S' {
+                start = (x, y)
+            }
+        }
     }
 
-    graph
+    (start, highest, graph)
+}
+
+fn can_traverse(a: &char, b: &char) -> bool {
+    let elevation_map: HashMap<char, usize> = [
+        ('a', 1),
+        ('b', 2),
+        ('c', 3),
+        ('d', 4),
+        ('e', 5),
+        ('f', 6),
+        ('g', 7),
+        ('h', 8),
+        ('i', 9),
+        ('j', 10),
+        ('k', 11),
+        ('l', 12),
+        ('m', 13),
+        ('n', 14),
+        ('o', 15),
+        ('p', 16),
+        ('q', 17),
+        ('r', 18),
+        ('s', 19),
+        ('t', 20),
+        ('u', 21),
+        ('v', 22),
+        ('w', 23),
+        ('x', 24),
+        ('y', 25),
+        ('z', 26),
+        ('S', 1),
+        ('E', 26),
+    ]
+    .iter()
+    .cloned()
+    .collect();
+
+    elevation_map.get(b).unwrap() <= &(elevation_map.get(a).unwrap() + 1)
 }
 
 fn main() {
--- src/bin/12/p1.rs
+++ src/bin/12/p1.rs
@@ -1,3 +1,48 @@
-pub fn solve() -> String {
-    String::from("Not implemented")
+use std::collections::HashMap;
+
+pub fn solve() -> f32 {
+    let (start, target, graph) = super::parse_input();
+
+    // println!("{:?}", graph);
+
+    let mut visited: Vec<&(usize, usize)> = Vec::new();
+    let mut unvisited: Vec<&(usize, usize)> = graph.keys().collect();
+    unvisited.sort();
+    unvisited.reverse();
+
+    let mut distances: HashMap<&(usize, usize), f32> =
+        graph.keys().map(|k| (k, f32::INFINITY)).collect();
+
+    distances.insert(&start, 0.0);
+
+    while !unvisited.is_empty() {
+        println!("{}", unvisited.len());
+        let copy = distances.to_owned();
+
+        let (v, _) = copy
+            .iter()
+            .filter(|(k, _)| unvisited.contains(k))
+            .min_by_key(|(_, v)| **v as u32)
+            .unwrap();
+
+        let v_i = unvisited.iter().position(|k| k == v).unwrap();
+        unvisited.remove(v_i);
+
+        for n in graph.get(*v).unwrap() {
+            if visited.contains(&n) {
+                continue;
+            }
+
+            let new_dis = distances.get(*v).unwrap() + 1.0;
+            let curr_dis = *distances.get(n).unwrap();
+
+            if new_dis < curr_dis {
+                distances.insert(n, new_dis);
+            }
+        }
+
+        visited.push(*v);
+    }
+
+    *distances.get(&target).unwrap()
 }
