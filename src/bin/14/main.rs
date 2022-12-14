7af150730f5186faf17ce8f7efa2ccee4ccb8008
Chris Holder <chris.holder@ramseysolutions.com>
2022-12-14 16:37:02 -0600

    Complete day 14, part 1

--- src/bin/14/main.rs
+++ src/bin/14/main.rs
@@ -1,13 +1,73 @@
 use std::fs;
 
+use regex::Regex;
+
 pub mod p1;
 pub mod p2;
 
-fn read_input() -> String {
-    fs::read_to_string("src/bin/14/input.txt").expect("Should have been able to read the file")
+fn parse_input() -> Vec<Vec<char>> {
+    let input =
+        fs::read_to_string("src/bin/14/input.txt").expect("Should have been able to read the file");
+
+    let re_x = Regex::new(r"(\d+),").unwrap();
+    let re_y = Regex::new(r",(\d+)").unwrap();
+
+    let x_bound = re_x
+        .captures_iter(&input[..])
+        .map(|c| c[0][..c[0].len() - 1].parse::<usize>().unwrap())
+        .max()
+        .unwrap();
+
+    let y_bound = re_y
+        .captures_iter(&input[..])
+        .map(|c| c[0][1..].parse::<usize>().unwrap())
+        .max()
+        .unwrap();
+
+    let mut grid: Vec<Vec<char>> = vec![vec!['.'; x_bound + 1000]; y_bound + 1];
+
+    input.lines().for_each(|l| {
+        let coors: Vec<(usize, usize)> = l
+            .split(" -> ")
+            .map(|s| {
+                let pair: Vec<usize> = s.split(",").flat_map(|c| c.parse()).collect();
+                (pair[0], pair[1])
+            })
+            .collect();
+
+        for w in coors.windows(2) {
+            let first = w[0];
+            let second = w[1];
+
+            if w.len() == 2 {
+                if first.0 == second.0 {
+                    let mn = first.1.min(second.1);
+                    let mx = first.1.max(second.1);
+
+                    for y in mn..mx + 1 {
+                        grid[y][first.0] = '#';
+                    }
+                } else if first.1 == second.1 {
+                    let mn = first.0.min(second.0);
+                    let mx = first.0.max(second.0);
+
+                    for x in mn..mx + 1 {
+                        grid[first.1][x] = '#';
+                    }
+                } else {
+                    panic!("bad coordinates: {:?},{:?}", first, second)
+                }
+            } else {
+                grid[first.1][first.0] = '#';
+            }
+        }
+    });
+
+    grid
 }
 
 fn main() {
+    parse_input();
     println!("Part 1: {}", p1::solve());
     println!("Part 2: {}", p2::solve());
 }
--- src/bin/14/p1.rs
+++ src/bin/14/p1.rs
@@ -1,3 +1,54 @@
-pub fn solve() -> String {
-    String::from("Not implemented")
+pub fn solve() -> u32 {
+    let mut grid = super::parse_input();
+    let mut sand_count = 0;
+
+    loop {
+        let mut sand = (500, 0);
+        sand_count += 1;
+
+        let mut done = false;
+
+        loop {
+            match find_next(&sand, &grid) {
+                Some(next) => sand = next,
+                None => {
+                    if sand.1 + 1 == grid.len() {
+                        done = true;
+                    }
+
+                    grid[sand.1][sand.0] = 'o';
+
+                    break;
+                }
+            }
+        }
+
+        if done {
+            break;
+        }
+    }
+
+    sand_count - 1
+}
+
+fn find_next(sand: &(usize, usize), grid: &Vec<Vec<char>>) -> Option<(usize, usize)> {
+    let (x, y) = sand;
+
+    if y + 1 == grid.len() {
+        return None;
+    }
+
+    if grid[y + 1][*x] == '.' {
+        return Some((*x, y + 1));
+    }
+
+    if grid[y + 1][x - 1] == '.' {
+        return Some((x - 1, y + 1));
+    }
+
+    if grid[y + 1][x + 1] == '.' {
+        return Some((x + 1, y + 1));
+    }
+
+    None
 }
