501e17d2ba18ae15a5cdb80f86a5d3e622b8cf20
Chris Holder <chris.holder@ramseysolutions.com>
2022-12-14 08:14:02 -0600

    Solve day 13, part 2

--- src/bin/13/p2.rs
+++ src/bin/13/p2.rs
@@ -2,7 +2,7 @@ use serde_json::Value;
 
 use crate::parse_input2;
 
-pub fn solve() -> i32 {
+pub fn solve() -> usize {
     let mut packets = parse_input2();
 
     let mut swap = false;
@@ -36,8 +36,28 @@ pub fn solve() -> i32 {
         }
     }
 
-    println!("{:?}", packets.iter().position(|v| {}));
-    0
+    (find_divider_packet(&packets, 6) + 1) * (find_divider_packet(&packets, 2) + 1)
+}
+
+fn find_divider_packet(packets: &Vec<Value>, divider: u64) -> usize {
+    packets
+        .iter()
+        .position(|p| {
+            if let Value::Array(outer) = p {
+                if outer.len() == 1 {
+                    if let Value::Array(inner) = &outer[0] {
+                        if inner.len() == 1 {
+                            if let Some(n) = inner[0].as_u64() {
+                                return n == divider;
+                            }
+                        }
+                    }
+                }
+            }
+
+            false
+        })
+        .unwrap()
 }
 
 fn compare(a: &Value, b: &Value) -> State {
@@ -127,93 +147,3 @@ enum State {
     Unsorted,
     Eq,
 }
-
-// fn is_sorted(p: &Pair) -> State {
-//     let mut index = 0;
-
-//     let left_packet = p.left.as_array().unwrap();
-//     let right_packet = p.right.as_array().unwrap();
-
-//     loop {
-//         // We've reached the end of both packets
-//         if index >= left_packet.len() && index >= right_packet.len() {
-//             return State::Eq;
-//         }
-
-//         if index >= left_packet.len() {
-//             return State::Sorted;
-//         }
-
-//         if index >= right_packet.len() {
-//             return State::Unsorted;
-//         }
-
-//         let left = &p.left[index];
-//         let right = &p.right[index];
-
-//         if left.is_number() && right.is_number() {
-//             let left = left.as_u64().unwrap();
-//             let right = right.as_u64().unwrap();
-
-//             if left < right {
-//                 return State::Sorted;
-//             } else if left > right {
-//                 return State::Unsorted;
-//             }
-
-//             index += 1;
-//             continue;
-//         }
-
-//         if left.is_number() {
-//             let left = serde_json::to_value(vec![left]).unwrap();
-//             let res = is_sorted_pair(&Pair {
-//                 left,
-//                 right: right.to_owned(),
-//             });
-
-//             match res {
-//                 State::Eq => {
-//                     index += 1;
-//                     continue;
-//                 }
-//                 _ => {
-//                     return res;
-//                 }
-//             };
-//         }
-
-//         if right.is_number() {
-//             let right = serde_json::to_value(vec![right]).unwrap();
-//             let res = is_sorted_pair(&Pair {
-//                 left: left.to_owned(),
-//                 right,
-//             });
-
-//             match res {
-//                 State::Eq => {
-//                     index += 1;
-//                     continue;
-//                 }
-//                 _ => {
-//                     return res;
-//                 }
-//             };
-//         }
-
-//         let res = is_sorted_pair(&Pair {
-//             left: left.to_owned(),
-//             right: right.to_owned(),
-//         });
-
-//         match res {
-//             State::Eq => {
-//                 index += 1;
-//                 continue;
-//             }
-//             _ => {
-//                 return res;
-//             }
-//         };
-//     }
-// }
