ceae0637d0471a2a7e1a1e5fefa423a84758ffd1
Chris Holder <chris.holder@ramseysolutions.com>
2022-12-15 20:03:28 -0600

    Solve day 15, part 1

--- src/bin/15/main.rs
+++ src/bin/15/main.rs
@@ -1,13 +1,69 @@
 use std::fs;
 
+use regex::Regex;
+
 pub mod p1;
 pub mod p2;
 
-fn read_input() -> String {
-    fs::read_to_string("src/bin/15/input.txt").expect("Should have been able to read the file")
+fn parse_input() -> Vec<Sensor> {
+    let input =
+        fs::read_to_string("src/bin/15/input.txt").expect("Should have been able to read the file");
+
+    let re =
+        Regex::new(r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)")
+            .unwrap();
+
+    input
+        .lines()
+        .map(|l| {
+            let caps = re.captures(l).unwrap();
+
+            let x1 = caps.get(1).unwrap().as_str().parse().unwrap();
+            let y1 = caps.get(2).unwrap().as_str().parse().unwrap();
+
+            let x2 = caps.get(3).unwrap().as_str().parse().unwrap();
+            let y2 = caps.get(4).unwrap().as_str().parse().unwrap();
+
+            Sensor {
+                location: Location { x: x1, y: y1 },
+                beacon: Beacon {
+                    location: Location { x: x2, y: y2 },
+                },
+                distance: x1.abs_diff(x2) + y1.abs_diff(y2),
+            }
+        })
+        .collect()
 }
 
 fn main() {
     println!("Part 1: {}", p1::solve());
     println!("Part 2: {}", p2::solve());
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+struct Location {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Beacon {
+    location: Location,
+}
+
+#[derive(Debug)]
+struct Sensor {
+    location: Location,
+    beacon: Beacon,
+    distance: u32,
+}
+
+impl Sensor {
+    fn get_min_range(&self) -> i32 {
+        self.location.y - self.distance as i32
+    }
+
+    fn get_max_range(&self) -> i32 {
+        self.location.y + self.distance as i32
+    }
 }
--- src/bin/15/p1.rs
+++ src/bin/15/p1.rs
@@ -1,3 +1,30 @@
-pub fn solve() -> String {
-    String::from("Not implemented")
+use std::collections::HashSet;
+
+use crate::Location;
+
+const ROW: i32 = 2_000_000;
+
+pub fn solve() -> usize {
+    let sensors = super::parse_input();
+
+    let beacon_locations: HashSet<Location> = sensors.iter().map(|s| s.beacon.location).collect();
+
+    let mut locations: HashSet<Location> = HashSet::new();
+
+    for s in &sensors {
+        if ROW as i32 >= s.get_min_range() && ROW as i32 <= s.get_max_range() {
+            let y_diff = ROW.abs_diff(s.location.y);
+
+            let x_min = s.location.x - (s.distance - y_diff) as i32;
+            let x_max = s.location.x + (s.distance - y_diff) as i32;
+
+            for x in x_min..x_max + 1 {
+                let l = Location { x, y: ROW as i32 };
+
+                locations.insert(l);
+            }
+        }
+    }
+
+    locations.difference(&beacon_locations).count()
 }
