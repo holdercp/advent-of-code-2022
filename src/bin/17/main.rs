b7c52851d9f45a2397ddc75694addc5855609461
Chris Holder <chris.holder@ramseysolutions.com>
2022-12-19 08:04:40 -0600

    Get rocks falling

--- src/bin/17/main.rs
+++ src/bin/17/main.rs
@@ -13,6 +13,7 @@ fn parse_input() -> Vec<JetPattern> {
         fs::read_to_string("src/bin/17/input.txt").expect("Should have been able to read the file");
 
     input
+        .trim()
         .chars()
         .map(|c| match c {
             '>' => JetPattern::Right,
@@ -26,34 +27,80 @@ fn get_shape_arr() -> [Shape; 5] {
     [Shape::Line, Shape::Plus, Shape::J, Shape::I, Shape::Square]
 }
 
+#[derive(Debug)]
 struct Rock {
+    r#type: String,
     points: Vec<Point>,
 }
 
 impl Rock {
-    fn new(s: Shape, p: Point) -> Self {
+    fn new(s: &Shape, y: &usize) -> Self {
         match s {
             Shape::Line => Self {
+                r#type: String::from("line"),
                 points: vec![
-                    Point(2, p.1 + 3),
-                    Point(3, p.1 + 3),
-                    Point(4, p.1 + 3),
-                    Point(5, p.1 + 3),
+                    Point(4, y + 3),
+                    Point(5, y + 3),
+                    Point(3, y + 3),
+                    Point(2, y + 3),
                 ],
             },
             Shape::Plus => Self {
+                r#type: String::from("plus"),
                 points: vec![
-                    Point(2, p.1 + 4),
-                    Point(3, p.1 + 3),
-                    Point(3, p.1 + 4),
-                    Point(3, p.1 + 5),
-                    Point(4, p.1 + 4),
+                    Point(3, y + 5),
+                    Point(4, y + 4),
+                    Point(3, y + 3),
+                    Point(2, y + 4),
+                    Point(3, y + 4),
+                ],
+            },
+            Shape::J => Self {
+                r#type: String::from("j"),
+                points: vec![
+                    Point(4, y + 5),
+                    Point(4, y + 3),
+                    Point(3, y + 3),
+                    Point(2, y + 3),
+                    Point(4, y + 4),
+                ],
+            },
+            Shape::I => Self {
+                r#type: String::from("i"),
+                points: vec![
+                    Point(2, y + 6),
+                    Point(2, y + 5),
+                    Point(2, y + 3),
+                    Point(2, y + 4),
+                ],
+            },
+            Shape::Square => Self {
+                r#type: String::from("square"),
+                points: vec![
+                    Point(2, y + 4),
+                    Point(3, y + 4),
+                    Point(3, y + 3),
+                    Point(2, y + 3),
                 ],
             },
         }
     }
+
+    fn top(&self) -> usize {
+        self.points[0].1
+    }
+    fn right(&self) -> usize {
+        self.points[1].0
+    }
+    fn bottom(&self) -> usize {
+        self.points[2].1
+    }
+    fn left(&self) -> usize {
+        self.points[3].0
+    }
 }
 
+#[derive(Debug)]
 struct Point(usize, usize);
 
 enum Shape {
--- src/bin/17/p1.rs
+++ src/bin/17/p1.rs
@@ -1,33 +1,37 @@
 use std::collections::HashSet;
 
-use crate::JetPattern;
+use crate::{JetPattern, Point, Rock};
 
-pub fn solve() -> String {
+pub fn solve() -> u32 {
     let patterns = super::parse_input();
     let shape_order = super::get_shape_arr();
 
     const LEFT_WALL: u32 = 0;
     const RIGHT_WALL: u32 = 6;
     const FLOOR: u32 = 0;
-    const LIMIT: u32 = 2022;
+    const LIMIT: usize = 2022;
 
     let mut rocks_fallen = 0;
     let mut tower_height = 0;
 
-    let mut chamber = Vec::new();
+    let mut chamber: Vec<Vec<Option<char>>> = Vec::new();
 
     loop {
-        let rock = build_rock(shape_order[rocks_fallen % 5]);
+        let rock = Rock::new(&shape_order[rocks_fallen % 5], &tower_height);
 
         rocks_fallen += 1;
         if rocks_fallen > LIMIT {
             break;
         }
+
+        println!("{:?}", rock);
         // Expand chamber by shape height
         // Calculate where shape will shift in 4 cycles
 
         // Update tower height
     }
+
+    0
 }
 
 enum Movement {
