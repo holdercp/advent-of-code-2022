be5846593c6756d299883ce6adf4a7174b259663
holdercp <25958393+holdercp@users.noreply.github.com>
2022-12-18 20:43:59 -0600

    Take a different approach to day 17

--- src/bin/17/main.rs
+++ src/bin/17/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, fs};
+use std::fs;
 
 pub mod p1;
 pub mod p2;
@@ -22,147 +22,49 @@ fn parse_input() -> Vec<JetPattern> {
         .collect()
 }
 
+fn get_shape_arr() -> [Shape; 5] {
+    [Shape::Line, Shape::Plus, Shape::J, Shape::I, Shape::Square]
+}
+
+struct Rock {
+    points: Vec<Point>,
+}
+
+impl Rock {
+    fn new(s: Shape, p: Point) -> Self {
+        match s {
+            Shape::Line => Self {
+                points: vec![
+                    Point(2, p.1 + 3),
+                    Point(3, p.1 + 3),
+                    Point(4, p.1 + 3),
+                    Point(5, p.1 + 3),
+                ],
+            },
+            Shape::Plus => Self {
+                points: vec![
+                    Point(2, p.1 + 4),
+                    Point(3, p.1 + 3),
+                    Point(3, p.1 + 4),
+                    Point(3, p.1 + 5),
+                    Point(4, p.1 + 4),
+                ],
+            },
+        }
+    }
+}
+
+struct Point(usize, usize);
+
+enum Shape {
+    Line,
+    Plus,
+    J,
+    I,
+    Square,
+}
+
 enum JetPattern {
     Left,
     Right,
-}
-
-#[derive(Hash, Eq, PartialEq, Clone, Copy)]
-struct Point {
-    x: u32,
-    y: u32,
-}
-
-struct Chamber {
-    border: HashSet<Point>,
-    floor: HashSet<Point>,
-    left_boundary: HashSet<Point>,
-    right_boundary: HashSet<Point>,
-}
-
-impl Chamber {
-    fn new(border: HashSet<Point>) -> Self {
-        Self { border }
-    }
-
-    fn update_border(points: HashSet<Point>) {}
-}
-
-trait Rock {
-    fn new(start: Point) -> Self;
-
-    fn fall(&mut self, floor: &HashSet<Point>) -> Result<HashSet<&Point>, HashSet<Point>>;
-
-    fn shift_left(&mut self, left_bound: &HashSet<Point>);
-
-    fn shift_right(&mut self, right_bound: &HashSet<Point>);
-
-    fn get_points(&self) -> HashSet<&Point>;
-}
-
-struct Line {
-    left: Point,
-    mid_left: Point,
-    mid_right: Point,
-    right: Point,
-}
-
-impl Rock for Line {
-    fn new(start: Point) -> Self {
-        let left = Point {
-            x: start.x - 3,
-            y: start.y,
-        };
-
-        let mid_left = Point {
-            x: start.x - 2,
-            y: start.y,
-        };
-
-        let mid_right = Point {
-            x: start.x - 1,
-            y: start.y,
-        };
-
-        Self {
-            left,
-            mid_left,
-            mid_right,
-            right: start,
-        }
-    }
-
-    fn fall(&mut self, floor: &HashSet<Point>) -> Result<HashSet<&Point>, HashSet<Point>> {
-        let has_collision = floor.iter().any(|f| {
-            self.get_points()
-                .iter()
-                .any(|p| f.x == p.x && f.y == p.y - 1)
-        });
-
-        if has_collision {
-            Err(self.get_points().into_iter().cloned().collect())
-        } else {
-            self.left.y -= 1;
-            self.mid_left.y -= 1;
-            self.mid_right.y -= 1;
-            self.right.y -= 1;
-
-            Ok(self.get_points())
-        }
-    }
-
-    fn shift_left(&mut self, bounds: &HashSet<Point>) {
-        let has_collision = bounds.iter().any(|b| {
-            self.get_points()
-                .iter()
-                .any(|p| b.y == p.y && b.x == p.x - 1)
-        });
-
-        self.left.x -= 1;
-        self.mid_left.x -= 1;
-        self.mid_right.x -= 1;
-        self.right.x -= 1;
-    }
-
-    fn shift_right(&mut self, bounds: &HashSet<Point>) {
-        let has_collision = bounds.iter().any(|b| {
-            self.get_points()
-                .iter()
-                .any(|p| b.y == p.y && b.x == p.x + 1)
-        });
-
-        self.left.x += 1;
-        self.mid_left.x += 1;
-        self.mid_right.x += 1;
-        self.right.x += 1;
-    }
-
-    fn get_points(&self) -> HashSet<&Point> {
-        HashSet::from([&self.left, &self.mid_left, &self.mid_right, &self.right])
-    }
-}
-
-struct Plus {
-    top_point: Point,
-    left_point: Point,
-    right_point: Point,
-    bottom_point: Point,
-}
-
-struct J {
-    top_point: Point,
-    left_point: Point,
-    right_point: Point,
-}
-
-struct I {
-    top_point: Point,
-    bottom_point: Point,
-}
-
-struct Block {
-    top_point: Point,
-    left_point: Point,
-    right_point: Point,
-    bottom_point: Point,
 }
--- src/bin/17/p1.rs
+++ src/bin/17/p1.rs
@@ -1,47 +1,32 @@
 use std::collections::HashSet;
 
-use crate::{JetPattern, Line, Point, Rock};
+use crate::JetPattern;
 
 pub fn solve() -> String {
     let patterns = super::parse_input();
+    let shape_order = super::get_shape_arr();
 
-    const LEFT_BOUND: u32 = 0;
-    const RIGHT_BOUND: u32 = 6;
+    const LEFT_WALL: u32 = 0;
+    const RIGHT_WALL: u32 = 6;
+    const FLOOR: u32 = 0;
+    const LIMIT: u32 = 2022;
 
-    let mut floor: HashSet<Point> = (0..7).map(|n| Point { x: n, y: 0 }).collect();
+    let mut rocks_fallen = 0;
+    let mut tower_height = 0;
 
-    let highest_point = floor.iter().max_by_key(|p| p.y).unwrap();
-
-    let mut falling_rock = Line::new(Point {
-        x: RIGHT_BOUND,
-        y: highest_point.y + 3,
-    });
-
-    let mut fallen_rocks: u32 = 1;
-    let mut movement = Movement::Jet;
-    let mut pattern_index: usize = 0;
+    let mut chamber = Vec::new();
 
     loop {
-        match movement {
-            Movement::Jet => {
-                let pattern = patterns.get(pattern_index % patterns.len()).unwrap();
+        let rock = build_rock(shape_order[rocks_fallen % 5]);
 
-                match pattern {
-                    JetPattern::Left => falling_rock.shift_left(LEFT_BOUND),
-                    JetPattern::Right => falling_rock.shift_right(RIGHT_BOUND),
-                };
+        rocks_fallen += 1;
+        if rocks_fallen > LIMIT {
+            break;
+        }
+        // Expand chamber by shape height
+        // Calculate where shape will shift in 4 cycles
 
-                pattern_index += 1;
-                movement = Movement::toggle(movement);
-            }
-            Movement::Fall => {
-                if let Err(points) = falling_rock.fall(&floor) {
-                    // TODO: Update floor
-                    floor = falling_rock = Line::new(start);
-                    fallen_rocks += 1;
-                };
-            }
-        };
+        // Update tower height
     }
 }
 
