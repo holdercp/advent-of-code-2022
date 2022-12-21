bc30083ecbe88cd288d429d89680a9f5962be8f3
Chris Holder <chris.holder@ramseysolutions.com>
2022-12-21 15:39:30 -0600

    Solve day 21, part 1

--- src/bin/21/main.rs
+++ src/bin/21/main.rs
@@ -1,13 +1,68 @@
-use std::fs;
+use std::{collections::HashMap, fs};
+
+use regex::Regex;
 
 pub mod p1;
 pub mod p2;
 
-fn read_input() -> String {
-    fs::read_to_string("src/bin/21/input.txt").expect("Should have been able to read the file")
+fn parse_input() -> HashMap<String, Monkey> {
+    let input =
+        fs::read_to_string("src/bin/21/input.txt").expect("Should have been able to read the file");
+
+    let re = Regex::new(r"(\w+): (\w+) ?(\+|-|/|\*)? ?(\w+)?").unwrap();
+
+    let mut monkeys = HashMap::new();
+
+    re.captures_iter(&input).for_each(|c| {
+        let name = c.get(1).unwrap().as_str();
+        let num_or_other = c.get(2).unwrap().as_str();
+
+        let monkey = if c.get(3).is_none() {
+            Monkey {
+                number: num_or_other.parse().unwrap(),
+                operation: Operation::None,
+                others: vec![],
+            }
+        } else {
+            let operation = c.get(3).unwrap().as_str();
+            let other = c.get(4).unwrap().as_str();
+
+            let operation = match operation {
+                "+" => Operation::Add,
+                "-" => Operation::Subtract,
+                "*" => Operation::Multiply,
+                "/" => Operation::Divide,
+                _ => panic!("bad op"),
+            };
+
+            Monkey {
+                operation,
+                others: vec![num_or_other.to_string(), other.to_string()],
+                number: 0,
+            }
+        };
+
+        monkeys.insert(name.to_string(), monkey);
+    });
+
+    monkeys
 }
 
 fn main() {
     println!("Part 1: {}", p1::solve());
     println!("Part 2: {}", p2::solve());
+}
+
+struct Monkey {
+    others: Vec<String>,
+    operation: Operation,
+    number: u64,
+}
+
+enum Operation {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    None,
 }
--- src/bin/21/p1.rs
+++ src/bin/21/p1.rs
@@ -1,3 +1,28 @@
-pub fn solve() -> String {
-    String::from("Not implemented")
+use std::collections::HashMap;
+
+use crate::{Monkey, Operation};
+
+pub fn solve() -> u64 {
+    let monkeys = super::parse_input();
+
+    let root = monkeys.get("root").unwrap();
+
+    compute_monkey(root, &monkeys)
+}
+
+fn compute_monkey(m: &Monkey, monkeys: &HashMap<String, Monkey>) -> u64 {
+    if m.others.is_empty() {
+        return m.number;
+    }
+
+    let other1 = monkeys.get(&m.others[0]).unwrap();
+    let other2 = monkeys.get(&m.others[1]).unwrap();
+
+    match m.operation {
+        Operation::Add => compute_monkey(other1, monkeys) + compute_monkey(other2, monkeys),
+        Operation::Subtract => compute_monkey(other1, monkeys) - compute_monkey(other2, monkeys),
+        Operation::Multiply => compute_monkey(other1, monkeys) * compute_monkey(other2, monkeys),
+        Operation::Divide => compute_monkey(other1, monkeys) / compute_monkey(other2, monkeys),
+        Operation::None => panic!("this shouldn't have happened"),
+    }
 }
