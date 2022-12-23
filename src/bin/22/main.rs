b99f706eb1f35c94781084fffc71d12fd75df80d
Chris Holder <chris.holder@ramseysolutions.com>
2022-12-23 16:24:17 -0600

    Scaffold day 22

---
+++ src/bin/22/main.rs
@@ -0,0 +1,13 @@
+use std::fs;
+
+pub mod p1;
+pub mod p2;
+
+fn read_input() -> String {
+    fs::read_to_string("src/bin/22/input.txt").expect("Should have been able to read the file")
+}
+
+fn main() {
+    println!("Part 1: {}", p1::solve());
+    println!("Part 2: {}", p2::solve());
+}
