65f88808e4424a85d42bfff400bd2aa184ab5795
Chris Holder <chris.holder@ramseysolutions.com>
2022-12-30 21:48:11 -0600

    Solve day 20, part 2

--- src/bin/20/p2.rs
+++ src/bin/20/p2.rs
@@ -1,3 +1,38 @@
-pub fn solve() -> String {
-    String::from("Not implemented")
+pub fn solve() -> i64 {
+    let file = super::parse_input();
+    let file: Vec<(i64, usize)> = file
+        .iter()
+        .map(|(n, i)| (*n as i64 * 811589153, *i))
+        .collect();
+
+    let mut mixed = file.clone();
+
+    for _ in 0..10 {
+        for num in &file {
+            let pos = mixed.iter().position(|n| n == num).unwrap();
+            let wrapped_pos = wrap(pos as i64 + num.0, mixed.len() - 1);
+
+            let n = mixed.remove(pos);
+
+            mixed.insert(wrapped_pos, n);
+        }
+    }
+
+    let pos0 = mixed.iter().position(|n| n.0 == 0).unwrap();
+
+    mixed[(pos0 + 1_000) % mixed.len()].0
+        + mixed[(pos0 + 2_000) % mixed.len()].0
+        + mixed[(pos0 + 3_000) % mixed.len()].0
+}
+
+fn wrap(n: i64, len: usize) -> usize {
+    let offset_count = if n.is_negative() {
+        (-n / len as i64) + 1
+    } else {
+        0
+    };
+
+    ((n + len as i64 * offset_count) % len as i64)
+        .try_into()
+        .unwrap()
 }
