b0ace4848f0fcdccf14ea3ad56dbc6f6e7d73a72
Chris Holder <chris.holder@ramseysolutions.com>
2022-12-30 21:41:05 -0600

    Solve day 20, part 1

--- src/bin/20/main.rs
+++ src/bin/20/main.rs
@@ -3,8 +3,15 @@ use std::fs;
 pub mod p1;
 pub mod p2;
 
-fn read_input() -> String {
-    fs::read_to_string("src/bin/20/input.txt").expect("Should have been able to read the file")
+fn parse_input() -> Vec<(i32, usize)> {
+    let input =
+        fs::read_to_string("src/bin/20/input.txt").expect("Should have been able to read the file");
+
+    input
+        .lines()
+        .enumerate()
+        .map(|(i, l)| (l.parse().unwrap(), i))
+        .collect()
 }
 
 fn main() {
--- src/bin/20/p1.rs
+++ src/bin/20/p1.rs
@@ -1,3 +1,32 @@
-pub fn solve() -> String {
-    String::from("Not implemented")
+pub fn solve() -> i32 {
+    let file = super::parse_input();
+
+    let mut mixed = file.clone();
+
+    for num in &file {
+        let pos = mixed.iter().position(|n| n == num).unwrap();
+        let wrapped_pos = wrap(pos as i32 + num.0, mixed.len() - 1);
+
+        let n = mixed.remove(pos);
+
+        mixed.insert(wrapped_pos, n);
+    }
+
+    let pos0 = mixed.iter().position(|n| n.0 == 0).unwrap();
+
+    mixed[(pos0 + 1_000) % mixed.len()].0
+        + mixed[(pos0 + 2_000) % mixed.len()].0
+        + mixed[(pos0 + 3_000) % mixed.len()].0
+}
+
+fn wrap(n: i32, len: usize) -> usize {
+    let offset_count = if n.is_negative() {
+        (-n / len as i32) + 1
+    } else {
+        0
+    };
+
+    ((n + len as i32 * offset_count) % len as i32)
+        .try_into()
+        .unwrap()
 }
