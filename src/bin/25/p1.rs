f73f0b26c616d8059fe636f6feeb4e17d752bdd1
Chris Holder <me@chrisholder.dev>
2023-11-19 09:51:09 -0600

    Get sum of snafu's

--- src/bin/25/p1.rs
+++ src/bin/25/p1.rs
@@ -1,23 +1,20 @@
-pub fn solve() -> Vec<i32> {
+pub fn solve() -> i32 {
     let fuel_requirements = super::read_input();
 
-    let sums = fuel_requirements
-        .lines()
-        .map(|r| {
-            r.chars().rev().enumerate().fold(0, |sum, c| {
-                let decimal: i32 = match c.1 {
-                    '2' => 2,
-                    '1' => 1,
-                    '0' => 0,
-                    '-' => -1,
-                    '=' => -2,
-                    _ => panic!("bad snafu digit"),
-                };
+    fuel_requirements.lines().fold(0, |sum, r| {
+        let converted_snafu = r.chars().rev().enumerate().fold(0, |sum, c| {
+            let decimal: i32 = match c.1 {
+                '2' => 2,
+                '1' => 1,
+                '0' => 0,
+                '-' => -1,
+                '=' => -2,
+                _ => panic!("bad snafu digit"),
+            };
 
-                sum + decimal * (5_i32.pow(u32::try_from(c.0).unwrap()))
-            })
-        })
-        .collect();
+            sum + decimal * (5_i32.pow(u32::try_from(c.0).unwrap()))
+        });
 
-    sums
+        sum + converted_snafu
+    })
 }
