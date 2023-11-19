9f594a2a7f11f8870e23462d9bf2a736e7bed23c
Chris Holder <me@chrisholder.dev>
2023-11-19 09:54:22 -0600

    Refactor to closure

--- src/bin/25/p1.rs
+++ src/bin/25/p1.rs
@@ -1,19 +1,21 @@
 pub fn solve() -> i32 {
     let fuel_requirements = super::read_input();
 
-    fuel_requirements.lines().fold(0, |sum, r| {
-        let converted_snafu = r.chars().rev().enumerate().fold(0, |sum, c| {
-            let decimal: i32 = match c.1 {
-                '2' => 2,
-                '1' => 1,
-                '0' => 0,
-                '-' => -1,
-                '=' => -2,
-                _ => panic!("bad snafu digit"),
-            };
+    let snafu_to_decimal = |sum: i32, snafu: (usize, char)| {
+        let decimal: i32 = match snafu.1 {
+            '2' => 2,
+            '1' => 1,
+            '0' => 0,
+            '-' => -1,
+            '=' => -2,
+            _ => panic!("bad snafu digit"),
+        };
 
-            sum + decimal * (5_i32.pow(u32::try_from(c.0).unwrap()))
-        });
+        sum + decimal * (5_i32.pow(u32::try_from(snafu.0).unwrap()))
+    };
+
+    fuel_requirements.lines().fold(0, |sum, r| {
+        let converted_snafu = r.chars().rev().enumerate().fold(0, snafu_to_decimal);
 
         sum + converted_snafu
     })
